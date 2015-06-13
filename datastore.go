da7440a9cca0ef3873b3f142f57141a78cd60dd7
Diego Borges <drborges.cic@gmail.com>
2015-06-13 02:07:09 -0300

    Implements happy path scenario for ds.Datastore.Put

--- datastore.go
+++ datastore.go
@@ -16,3 +16,18 @@ func (this Datastore) Load(p Persistable) error {
 
 	return datastore.Get(this.Context, p.Key(), p)
 }
+
+func (this Datastore) Put(p Persistable) error {
+	if err := ResolveKey(this.Context, p); err != nil {
+		return err
+	}
+
+	key, err := datastore.Put(this.Context, p.Key(), p)
+
+	if err != nil {
+		return err
+	}
+
+	p.SetKey(key)
+	return nil
+}
