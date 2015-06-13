cf4ccbfffe1100a71801ef225eb73fdef2dcf9b2
Diego Borges <drborges.cic@gmail.com>
2015-06-13 01:47:57 -0300

    Fails load for models missing auto generated key

--- api.go
+++ api.go
@@ -2,33 +2,31 @@ package ds
 
 import "appengine/datastore"
 
-type (
-	Persistable interface {
-		KeyMetadata() *KeyMetadata
-		Key() *datastore.Key
-		SetKey(*datastore.Key)
-	}
+type Persistable interface {
+	KeyMetadata() *KeyMetadata
+	Key() *datastore.Key
+	SetKey(*datastore.Key)
+}
 
-	CacheableModel interface {
-		Persistable
-		CacheKey() string
-	}
+type CacheableModel interface {
+	Persistable
+	CacheKey() string
+}
 
-	Putter interface {
-		Put(Persistable) error
-	}
+type Putter interface {
+	Put(Persistable) error
+}
 
-	Loader interface {
-		Load(Persistable) error
-	}
+type Loader interface {
+	Load(Persistable) error
+}
 
-	Deleter interface {
-		Delete(Persistable) error
-	}
+type Deleter interface {
+	Delete(Persistable) error
+}
 
-	Datasource interface {
-		Putter
-		Loader
-		Deleter
-	}
-)
+type Datasource interface {
+	Putter
+	Loader
+	Deleter
+}
