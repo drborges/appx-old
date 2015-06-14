1f87635feaa513221ceadf29ca9691015dc7ec42
Diego Borges <drborges.cic@gmail.com>
2015-06-14 13:55:54 -0300

    Deletes dsx package

--- dsx/errors.go
+++
@@ -1,8 +0,0 @@
-package dsx
-
-import "errors"
-
-var (
-	ErrMissingStringID = errors.New(`Model is missing StringId. String field tagged with db:"id" cannot be empty`)
-	ErrMissingIntID    = errors.New(`Model is missing IntId. Integer field tagged with db:"id" cannot be zero`)
-)
