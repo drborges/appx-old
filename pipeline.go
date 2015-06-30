57cbc708a003cddf271a23d2dc8b26eb4e9cbfba
Diego Borges <drborges.cic@gmail.com>
2015-06-30 01:54:52 -0300

    Interfaces based pipeline with chainnable API

--- pipeline.go
+++ pipeline.go
@@ -1,81 +1,202 @@
 package appx
 
 import (
-	"errors"
 	"appengine"
+	"errors"
 )
 
 var ErrEmptySlice = errors.New("Cannot produce entities from an empty slice")
 
-// Produce emits entities in the output channel until
-// there is no more closing the channel at the end
-type Producer func(status chan<- error) <-chan Entity
+type Producer interface {
+	BufferSize() int
+	Produce(status chan<- error) <-chan Entity
+}
 
-// Consume consumes incoming entities from the in channel
-// processes the entity emitting the result into the output
-// channel which gets closed when there is no more incoming entities
-type Consumer func(in <-chan Entity, status chan<- error) <-chan Entity
+type Consumer interface {
+	SetBufferSize(size int)
+	Consume(in <-chan Entity)
+}
 
-// Collect collects the pipeline result returning an error in case
-// any consumer emits an error message after closing its output channel
-type Collect func(in <-chan Entity, status <-chan error) error
+type ConsumerProducer interface {
+	Consumer
+	Producer
+}
 
-type Consume func (in <- chan Entity, out chan <- Entity, status chan <- error)
+type Pipeline struct {
+	producer  Producer
+	consumers []ConsumerProducer
+	status    <-chan error
+	out       <-chan Entity
+}
 
-func NewProducer(entities ...Entity) Producer {
-	return func(status chan<- error) <-chan Entity {
-		out := make(chan Entity, len(entities))
-
-		go func() {
-			defer close(out)
-			if len(entities) == 0 {
-				Emit(status, ErrEmptySlice)
-				return
-			}
-
-			for _, e := range entities {
-				out <- e
-			}
-
-			Emit(status, Done)
-		}()
-
-		return out
+func NewPipeline(producer Producer) *Pipeline {
+	return &Pipeline{
+		producer:  producer,
+		consumers: []ConsumerProducer{},
 	}
 }
 
-func NewKeyResolver(context appengine.Context, bufsize int) Consumer {
-	return NewConsumer(bufsize, func (in <- chan Entity, out chan <- Entity, status chan <- error) {
-		for entity := range in {
-			if err := ResolveKey(context, entity); err != nil {
-				Emit(status, err)
+func (this *Pipeline) Then(consumer ConsumerProducer) *Pipeline {
+	this.consumers = append(this.consumers, consumer)
+	return this
+}
+
+func (this *Pipeline) Run() *Pipeline {
+	status := make(chan error, len(this.consumers)+1)
+	out := this.producer.Produce(status)
+	for _, consumer := range this.consumers {
+		consumer.SetBufferSize(this.producer.BufferSize())
+		consumer.Consume(out)
+		out = consumer.Produce(status)
+	}
+	this.status = status
+	this.out = out
+	return this
+}
+
+func (this *Pipeline) CollectStatus() error {
+	return NewStatusCollector(this.status).Collect()
+}
+
+func (this *Pipeline) CollectEntities() []Entity {
+	return NewEntitiesCollector(this.out).Collect()
+}
+
+type EntityProducer struct {
+	Stage
+	entities []Entity
+}
+
+func NewEntityProducer(entities ...Entity) *EntityProducer {
+	return &EntityProducer{entities: entities}
+}
+
+func (this *EntityProducer) BufferSize() int {
+	return len(this.entities)
+}
+
+func (this *EntityProducer) Produce(status chan<- error) <-chan Entity {
+	return NewPipelineStage(len(this.entities), func(out chan<- Entity) {
+		if len(this.entities) == 0 {
+			this.Emit(status, ErrEmptySlice)
+			return
+		}
+
+		for _, e := range this.entities {
+			out <- e
+		}
+
+		this.Emit(status, Done)
+	})
+}
+
+type Stage struct {
+	buffSize int
+	in       <-chan Entity
+}
+
+func (this *Stage) SetBufferSize(size int) {
+	this.buffSize = size
+}
+
+func (this *Stage) BufferSize() int {
+	return this.buffSize
+}
+
+func (this *Stage) Consume(in <-chan Entity) {
+	this.in = in
+}
+
+// Emit emits a pipeline stage result within a
+// goroutine to be handled thereafter
+func (this *Stage) Emit(status chan<- error, s error) {
+	go func() {
+		status <- s
+	}()
+}
+
+type KeyResolverStage struct {
+	Stage
+	context  appengine.Context
+}
+
+func NewKeyResolverStage(c appengine.Context) ConsumerProducer {
+	return &KeyResolverStage{
+		context:  c,
+	}
+}
+
+func (this *KeyResolverStage) Produce(status chan<- error) <-chan Entity {
+	return NewPipelineStage(this.buffSize, func(out chan<- Entity) {
+		for entity := range this.in {
+			if err := ResolveKey(this.context, entity); err != nil {
+				this.Emit(status, err)
 				return
 			}
 			out <- entity
 		}
 
-		Emit(status, Done)
+		this.Emit(status, Done)
 	})
 }
 
-func NewConsumer(bufsize int, consume Consume) Consumer {
-	return func(in <-chan Entity, status chan<- error) <-chan Entity {
-		out := make(chan Entity, bufsize)
+type StatusCollector struct {
+	statuses   <-chan error
+	stageCount int
+}
 
-		go func() {
-			defer close(out)
-			consume(in, out, status)
-		}()
-
-		return out
+func NewStatusCollector(statuses <-chan error) *StatusCollector {
+	return &StatusCollector{
+		statuses:   statuses,
+		stageCount: 1,
 	}
 }
 
-// Emit emits a pipeline stage result within a
-// goroutine to be handled thereafter
-func Emit(status chan <- error, s error) {
-	go func () {
-		status <- s
+func (this *StatusCollector) SetStageCount(count int) *StatusCollector {
+	this.stageCount = count
+	return this
+}
+
+func (this *StatusCollector) Collect() error {
+	finalStatus := Done
+
+	for i := 0; i < this.stageCount; i++ {
+		if err := <-this.statuses; err != Done {
+			finalStatus = err
+		}
+	}
+
+	return finalStatus
+}
+
+type EntitiesCollector struct {
+	entities <-chan Entity
+}
+
+func NewEntitiesCollector(entities <-chan Entity) *EntitiesCollector {
+	return &EntitiesCollector{
+		entities: entities,
+	}
+}
+
+func (this *EntitiesCollector) Collect() []Entity {
+	entities := []Entity{}
+
+	for entity := range this.entities {
+		entities = append(entities, entity)
+	}
+
+	return entities
+}
+
+func NewPipelineStage(buffSize int, handler func(out chan<- Entity)) chan Entity {
+	out := make(chan Entity, buffSize)
+
+	go func() {
+		defer close(out)
+		handler(out)
 	}()
+
+	return out
 }
 
--- pipeline_test.go
+++ pipeline_test.go
@@ -12,52 +12,129 @@ func TestPipeline(t *testing.T) {
 	c, _ := aetest.NewContext(nil)
 	defer c.Close()
 
-	Convey("Given I have a few entities", t, func() {
-		golang := &Tag{Name: "golang"}
-		swift := &Tag{Name: "swift"}
-		scala := &Tag{Name: "scala"}
+	Convey("Pipeline", t, func() {
+		Convey("Given I have a Producer ready to produce entities", func() {
+			golang := &Tag{Name: "golang"}
+			swift := &Tag{Name: "swift"}
+			scala := &Tag{Name: "scala"}
 
-		Convey("When I use a Producer to emit them into a channel", func() {
-			errs := make(chan error)
+			producer := appx.NewEntityProducer(golang, swift, scala)
 
-			emitted := []appx.Entity{}
-			for entity := range appx.NewProducer(golang, swift, scala)(errs) {
-				emitted = append(emitted, entity)
-			}
-
-			Convey("Then producer finishes successfully", func() {
-				So(<-errs, ShouldEqual, appx.Done)
+			Convey("When I produce the entities", func() {
+				emitted := []appx.Entity{}
+				status := make(chan error)
+				for entity := range producer.Produce(status) {
+					emitted = append(emitted, entity)
+				}
 
 				Convey("Then all entities are emitted", func() {
-					So(emitted, ShouldResemble, []appx.Entity{golang, swift, scala})
+					So(emitted, ShouldResemble, []appx.Entity{
+						golang,
+						swift,
+						scala,
+					})
+
+					Convey("Then the producer emits the Done status", func() {
+						So(<-status, ShouldEqual, appx.Done)
+					})
 				})
 			})
 		})
 
-		Convey("When I use a Producer to emit entities from a nil slice", func() {
-			errs := make(chan error)
+		Convey("Given I have a Producer with no entities to produce", func() {
+			producer := appx.NewEntityProducer()
 
-			for item := range appx.NewProducer()(errs) {
-				panic(fmt.Sprintf("Should not process %+v", item))
-			}
+			Convey("When I attempt to produce entities", func() {
+				status := make(chan error)
+				for entity := range producer.Produce(status) {
+					panic(fmt.Sprintf("Should not process %+v", entity))
+				}
 
-			Convey("Then an error is emitted", func() {
-				So(<-errs, ShouldEqual, appx.ErrEmptySlice)
+				Convey("Then an error is emitted", func() {
+					So(<-status, ShouldEqual, appx.ErrEmptySlice)
+				})
 			})
 		})
 
-		Convey("When I successfully consume produced entities with a given consumer", func() {
-			errs := make(chan error)
-			out := appx.NewProducer(golang, swift, scala)(errs)
+		Convey("Given I have a ConsumerProducer ready to consume entities", func() {
+			golang := &Tag{Name: "golang"}
+			swift := &Tag{Name: "swift"}
+			scala := &Tag{Name: "scala"}
 
-			Convey("Then all emitted entities are consumed", func() {
-				for item := range appx.NewKeyResolver(c, 3)(out, errs) {
-					So(item.EntityKey(), ShouldNotBeNil)
-				}
+			status := make(chan error)
+			entities := []appx.Entity{golang, swift, scala}
+			out := appx.NewEntityProducer(entities...).Produce(status)
+			consumerProducer := appx.NewKeyResolverStage(c)
+			consumerProducer.SetBufferSize(len(entities))
+			consumerProducer.Consume(out)
 
-				Convey("Then all emitted entities are consumed", func() {
-					So(<-errs, ShouldEqual, appx.Done)
-					So(<-errs, ShouldEqual, appx.Done)
+			Convey("When I call produce on the ConsumerProducer", func() {
+				out = consumerProducer.Produce(status)
+
+				Convey("And I collect the pipeline resulting successful status", func() {
+					stageCount := 2
+					statusCollector := appx.NewStatusCollector(status).SetStageCount(stageCount)
+					result := statusCollector.Collect()
+
+					Convey("Then all entities are consumed", func() {
+						for entity := range out {
+							So(entity.EntityKey(), ShouldNotBeNil)
+						}
+
+						Convey("Then all stages emit their status (Producer + Consumer)", func() {
+							So(result, ShouldEqual, appx.Done)
+						})
+					})
+				})
+
+				Convey("And I successfully collect all pipeline resulting entities", func() {
+					entities := appx.NewEntitiesCollector(out).Collect()
+
+					Convey("Then all emitted entities are consumed", func() {
+						So(entities, ShouldResemble, []appx.Entity{
+							golang, swift, scala,
+						})
+					})
+				})
+
+				Convey("Then all entities are consumed", func() {
+					for entity := range out {
+						So(entity.EntityKey(), ShouldNotBeNil)
+					}
+
+					Convey("Then all stages emit their status (Producer + Consumer)", func() {
+						So(<-status, ShouldEqual, appx.Done)
+						So(<-status, ShouldEqual, appx.Done)
+					})
+				})
+			})
+		})
+
+		Convey("Given I have a new pipeline with a prducer and a consumer", func() {
+			golang := &Tag{Name: "golang"}
+			swift := &Tag{Name: "swift"}
+			scala := &Tag{Name: "scala"}
+			entities := []appx.Entity{golang, swift, scala}
+
+			producer := appx.NewEntityProducer(entities...)
+			consumerProducer := appx.NewKeyResolverStage(c)
+			consumerProducer.SetBufferSize(len(entities))
+
+			pipeline := appx.NewPipeline(producer).Then(consumerProducer).Run()
+
+			Convey("When I collect the pipeline resulting status", func() {
+				status := pipeline.CollectStatus()
+
+				Convey("Then the pipeline finishes successfully", func() {
+					So(status, ShouldEqual, appx.Done)
+				})
+			})
+
+			Convey("When I collect the pipeline resulting entities", func() {
+				collectedEntities := pipeline.CollectEntities()
+
+				Convey("Then all entities pass through the pipeline", func() {
+					So(collectedEntities, ShouldResemble, entities)
 				})
 			})
 		})
